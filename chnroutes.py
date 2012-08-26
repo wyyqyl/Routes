2cd76defd516c80cf85730f1a5881a9bfd52aabd
Yorath Wang <yorathwang@gmail.com>
2012-08-26 14:07:03 +0800

    Add ip range exceptions
    you can specify the ip range you don't want to use vpn

--- chnroutes.py
+++ chnroutes.py
@@ -2,24 +2,44 @@
 
 import re
 import urllib2
-import math
 
 def generate_win():
 	results = fetch_ip_data()
 
-	upfile=open('chnroutesAdd','w')
-	downfile=open('chnroutesDel','w')
+	upfile = open('addchnroutes.txt', 'w')
+	downfile = open('delchnroutes.txt', 'w')
 
-	for ip, mask, _ in results:
+	for ip, mask in results:
 		upfile.write('add %s mask %s default METRIC default IF default\n' % (ip, mask))
 		downfile.write('delete %s mask %s default METRIC default IF default\n' % (ip, mask))
-
+	
 	upfile.close()
 	downfile.close()
 
 	print "Generate data done"
-
+	
+def add_exception():
+	results = []
+	try:
+		with open('exceptions.txt', 'r+') as exceptions:
+			for item in exceptions:
+				idx = item.find('#')
+				if idx > -1:
+					item = item[0:idx]
+				item = item.rstrip(' \t\r\n')
+				unit_items = item.split(' ')
+				if len(unit_items) != 2:
+					continue
+				starting_ip = unit_items[0]
+				mask = unit_items[1]
+				results.append((starting_ip, mask))
+		return results
+	except IOError as e:
+		return results
+	
 def fetch_ip_data():
+	results = add_exception()
+	
 	# fetch data from apnic
 	print "Fetching data from apnic.net, it might take a few minutes, please wait..."
 	url = r'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
@@ -27,9 +47,7 @@ def fetch_ip_data():
 
 	cnregex = re.compile(r'apnic\|cn\|ipv4\|[0-9\.]+\|[0-9]+\|[0-9]+\|a.*', re.IGNORECASE)
 	cndata = cnregex.findall(data)
-
-	results=[]
-
+	
 	for item in cndata:
 		unit_items = item.split('|')
 		starting_ip = unit_items[3]
@@ -48,12 +66,10 @@ def fetch_ip_data():
 		mask = [ int(i,16 ) for i in mask] 
 		mask = "%d.%d.%d.%d" % tuple(mask)
 		
-		# mask in *nix format
-		# mask2 = 32-int(math.log(num_ip,2))
-		
-		results.append(starting_ip,mask)
-		 
+		results.append((starting_ip, mask))
+	
 	return results
 	
-if __name__=='__main__':
+if __name__ == '__main__':
 	generate_win()
+	# add_exception()
